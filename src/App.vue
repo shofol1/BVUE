afe5ad5455e74fe1140dd6676c0df0480e40a718
shofol <18103156@iubat.edu>
2022-08-01 14:18:28 +0600

    data fetch with axios

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,25 @@
 <script>
 import HookVue from "./components/Hook.vue";
+import axios from "axios";
 
 export default {
   name: "App",
   data() {
-    return {};
+    return {
+      users: [],
+    };
+  },
+  // beforeCreate() {
+  //   alert("before create");
+  // },
+  // created() {
+  //   alert("after create");
+  // },
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => (this.users = res.data))
+      .catch((err) => console.log(err));
   },
   components: {
     HookVue,
@@ -15,4 +30,7 @@ export default {
 
 <template>
   <HookVue></HookVue>
+  <ul v-for="(user, i) in users" :key="user.id">
+    <li>{{ user.name }}</li>
+  </ul>
 </template>
